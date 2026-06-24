bcc3ef1bfeb3ace55a62829ce56a38a041d38a7e
L3BR0NJ4M3S <lebronjamesabong@gmail.com>
2026-06-24 15:54:05 +0800

    Add files via upload

---
+++ Abong_ED_A/Prelim_projects/js_calculator/js/script.js
@@ -0,0 +1,13 @@
+function calculateSum(){
+    const num1 =document.gentElementById('num1').value;
+
+    const num2 =document.gentElementById('num2').value;
+    
+    const number1 = parseFlot(num1);
+     const number1 = parseFlot(num1);
+
+     const sum =number1 + number2
+
+     document.getElementById('result').textContent='The sum is'+sum;
+
+}
